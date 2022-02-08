e2f7c366fe115ea9743265fd2952a2a9e6ea37ee
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-07 21:31:19 -0500

    add input bar

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
 
-class Home extends Component {
-  render() {
-    return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Project Home</h1>
-      {/* Link to List.js */}
-      <Link to={'./list'}>
-        <button variant="raised">
-            My List
-        </button>
-      </Link>
-    </div>
-    );
+// Creating a custom hook
+function useInput(defaultValue) {
+  const [value, setValue] = useState(defaultValue);
+  function onChange(e) {
+    setValue(e.target.value);
   }
+  return {
+    value,
+    onChange,
+  };
 }
-export default Home;
+
+export default function Home() {
+  const inputProps = useInput();
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Shorten the URL</h1>
+      <div>
+        <input {...inputProps} placeholder="Enter long url" />
+        <span>Value: {inputProps.value} </span>
+      </div>
+      <button variant="raised">ENTER</button>
+    </div>
+  );
+}
