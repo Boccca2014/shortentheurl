8a394c3a0320c803dc2892eb907cafbaec373557
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-08 10:24:18 -0500

    tailwind works, textbox works

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,28 +1,56 @@
 import React, { useState } from "react";
+import axios from "axios";
 // import { Link } from "react-router-dom";
 
-// Creating a custom hook
-function useInput(defaultValue) {
-  const [value, setValue] = useState(defaultValue);
-  function onChange(e) {
-    setValue(e.target.value);
-  }
-  return {
-    value,
-    onChange,
-  };
-}
-
 export default function Home() {
-  const inputProps = useInput();
+  const [urlInput, setUrlInput] = useState({
+    longUrl: "",
+  });
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setUrlInput({ ...urlInput, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Submitted", urlInput);
+    // axios
+    //   .post("/api/shorten", {
+    //     longUrl: value,
+    //   })
+    //   .then((res) => {
+    //     console.log("res", res);
+    //   });
+    setUrlInput({
+      longUrl: "",
+    });
+  };
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Shorten the URL</h1>
-      <div>
-        <input {...inputProps} placeholder="Enter long url" />
-        <span>Value: {inputProps.value} </span>
+    <div className="text-center">
+      <h1 className="text-5xl font-bold pt-4">Shorten the URL</h1>
+      <div className="text-center py-5">
+        <input
+          className="placeholder:italic placeholder:text-slate-400 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Enter long url"
+          type="text"
+          onChange={handleChange}
+          id="longUrl"
+          name="longUrl"
+          value={urlInput.longUrl}
+        />
       </div>
-      <button variant="raised">ENTER</button>
+      <button
+        className="rounded-xl border-black border-2 px-2 bg-emerald-500 text-white hover:bg-emerald-700 active:bg-violet-700"
+        onClick={handleSubmit}
+      >
+        ENTER
+      </button>
+      <div className="text-center py-5">
+        <span>Short URL: PLACEHOLDER </span>
+        {/* {inputProps.value} */}
+      </div>
     </div>
   );
 }
