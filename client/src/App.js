15526194b1532793a663bae5453f915fcce4a73d
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-07 21:25:23 -0500

    add favicons

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 /* client/src/App.js. */
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Home from './views/Home';
-import List from './views/List';
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./views/Home";
+import List from "./views/List";
 // import Links from './views/Links';
 
 export default function App() {
-    const App = () => (
-      <div>
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/list' component={List}/>
-        </Switch>
-      </div>
-    );
-  
-    return (
+  const App = () => (
+    <div>
       <Switch>
-        <App/>
+        <Route exact path="/" component={Home} />
+        <Route path="/list" component={List} />
       </Switch>
-    );
+    </div>
+  );
+
+  return (
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
 }
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
 class Home extends Component {
   render() {
     return (
-    <div className="App">
-      <h1>Project Home</h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Project Home</h1>
       {/* Link to List.js */}
       <Link to={'./list'}>
         <button variant="raised">
