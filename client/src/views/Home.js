e19b0f42e66bead5c8b32b68cb0471566eded46f
Sebastian Cabrejos <14218992+Boccca2014@users.noreply.github.com>
2022-02-08 20:20:42 -0500

    changed css, attempted to implement keypress

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -39,29 +39,41 @@ export default function Home() {
     }
   };
 
+  // const handleKeyPress = (e) => {
+  //   console.log("hello");
+  //   //it triggers by pressing the enter key
+  //   if (e.keyCode === 13 || e.code === "Enter" || e.code === "NumpadEnter") {
+  //     handleSubmit();
+  //   }
+  // };
+
   return (
     <div className="text-center">
       <h1 className="text-5xl font-bold pt-4">Shorten the URL</h1>
       <div className="text-center py-5">
         <input
-          className="placeholder:italic placeholder:text-slate-400 bg-white border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-          placeholder="Enter long url"
+          className="placeholder:italic placeholder:text-slate-400 bg-white border border-slate-300 rounded-md py-2 px-2 w-6/12 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Enter any long url"
           type="text"
           onChange={handleChange}
+          // onKeyPress={handleKeyPress}
           id="longUrl"
           name="longUrl"
           value={urlInput.longUrl}
         />
       </div>
       <button
-        className="rounded-xl border-black border-1 px-2 bg-emerald-500 text-white hover:bg-emerald-700 active:bg-violet-700"
+        className="rounded-xl border-black border-1 px-5 py-2 bg-emerald-500 text-white hover:bg-emerald-700 active:bg-violet-700"
         onClick={handleSubmit}
       >
         ENTER
       </button>
-      <div className="text-center py-5">
-        <span>Short URL: {urlInput.shortUrl} </span>
+      <div className="text-center py-10">
+        <h1 className="font-bold text-xl">Short URL: </h1>
       </div>
+      <span className="text-red-500 font-bold text-3xl">
+        {urlInput.shortUrl}
+      </span>
     </div>
   );
 }
